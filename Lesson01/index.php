8c29edf13e384877d1ab6e2dfcaa2e9d4880b756
mizushima-runa <runa.inoue065@gmail.com>
2024-10-05 03:39:36 +0900

    firstcommit

---
+++ Lesson01/index.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Hello World!";
+
+    echo "PHPのカリキュラム";
+
+    echo 1 + 2;
+    echo 10 - 5;
+    echo 3 * 2;
+    echo 10 / 2;
+?>
---
+++ Lesson02/index.php
@@ -0,0 +1,20 @@
+<?php
+/*lesson2*/
+$firstname = '水嶋';
+$name = '瑠菜';
+echo $firstname;
+echo $name.'<br/>';
+
+$number1 = 20;
+$number2 = 5;
+
+$plus = $number1 + $number2;
+$minus = $number1 - $number2;
+$multiplication = $number1 * $number2;
+$division = $number1 / $number2;
+
+echo $plus.'<br/>';
+echo $minus.'<br/>';
+echo $multiplication.'<br/>';
+echo $division.'<br/>';
+?>
